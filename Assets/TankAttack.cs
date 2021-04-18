80d303432959509161f0319acf731a7d8d3a5613
1694434266 <1977275620@qq.com>
2021-04-18 12:41:44 +0800

    First Commit

---
+++ Assets/TankAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankAttack : MonoBehaviour
+{
+    public GameObject shellPrefab;
+    public KeyCode fireKey = KeyCode.Space;
+    public float shellSpeed = 15;
+    public AudioClip shotAudio;
+    private Transform firePosition;
+    void Start()
+    {
+        firePosition = transform.Find("FirePosition");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(fireKey))
+        {
+            AudioSource.PlayClipAtPoint(shotAudio, transform.position);
+           GameObject go=GameObject.Instantiate(shellPrefab, firePosition.position, firePosition.rotation)as GameObject;
+            go.GetComponent<Rigidbody>().velocity = go.transform.forward *shellSpeed;
+         }
+        
+    }
+}
